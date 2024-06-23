00776838c64dadc7e7faccff782c4586eabae6a2
dhruvgupta794 <88080143+dhruvgupta794@users.noreply.github.com>
2024-06-23 16:17:15 +0530

    initial commit

---
+++ src/data/index.js
@@ -0,0 +1,52 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Ananya',
+    lastName: 'Singh',
+    email: 'ananya@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Vijay',
+    lastName: 'Patel',
+    email: 'vijay@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Rohit',
+    lastName: 'Sharma',
+    email: 'rohit@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Kavita',
+    lastName: 'Iyer',
+    email: 'kavita@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Amit',
+    lastName: 'Mehta',
+    email: 'amit@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Pooja',
+    lastName: 'Rao',
+    email: 'pooja@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+];
+
+export { employeesData };
